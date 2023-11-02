3053bb67de50e4959aa5ed2e828e6d77cceb8350
Jerald Lee <jleeshancheng@gmail.com>
2023-11-02 12:40:26 +0800

    Comment and SelectedNode
    Adding of comment button to the bottom navbar and having a variable which stores the current selected node's id

--- src/components/_react_flow/PostItMainPage.tsx
+++ src/components/_react_flow/PostItMainPage.tsx
@@ -1,193 +1,261 @@
-import { api } from '@/utils/api'; // import api to use trpc APIs
-import React, { useState, useRef, useCallback, useEffect } from 'react'; // import react hooks
+import { api } from "@/utils/api"; // import api to use trpc APIs
+import React, { useState, useRef, useCallback, useEffect } from "react"; // import react hooks
 import ReactFlow, {
-    MiniMap,
-    Controls,
-    Background,
-    useEdgesState,
-    addEdge,
-    Connection,
-    Edge,
-    BackgroundVariant,
-    useReactFlow,
-    Panel,
-    ReactFlowInstance,
-    applyNodeChanges,
-    ProOptions,
-    NodeDragHandler,
-    SelectionDragHandler,
-    OnNodesDelete,
-    OnEdgesDelete
-} from 'reactflow';// import required props from reactflow
-import 'reactflow/dist/style.css';
+  MiniMap,
+  Controls,
+  Background,
+  useEdgesState,
+  addEdge,
+  Connection,
+  Edge,
+  BackgroundVariant,
+  useReactFlow,
+  Panel,
+  ReactFlowInstance,
+  applyNodeChanges,
+  ProOptions,
+  NodeDragHandler,
+  SelectionDragHandler,
+  OnNodesDelete,
+  OnEdgesDelete,
+} from "reactflow"; // import required props from reactflow
+import "reactflow/dist/style.css";
 
 import { Button } from "@/components/ui/button";
-import useUndoRedo from '@/hooks/useUndoRedo'; //import custom hook for undo redo
+import useUndoRedo from "@/hooks/useUndoRedo"; //import custom hook for undo redo
 import { PostItNodeElement } from "@/Types/postIt"; // import type for postit node
 import PostItNode from "@/components/_react_flow/PostItNode"; // import custom node for postit node
 import { useData } from "@/context/postItContext"; // import useData for share state globally for all components
-import { on } from 'events';
-import Header from './Header';
+import { on } from "events";
+import Header from "./Header";
+import Comment from "./Comment";
 
 // create new node type for reactflow
 const nodeTypes = {
-    postItNode: PostItNode,
+  postItNode: PostItNode,
 };
 
-const proOptions: ProOptions = { account: 'paid-pro', hideAttribution: true };
+const proOptions: ProOptions = { account: "paid-pro", hideAttribution: true };
 
 export default () => {
-    const reactFlowWrapper = useRef<HTMLDivElement>(null);
-    const id: string = "clo6tzkzs0000us4wi39z9qld"; // for demo purposes, now we use default id. in the production environment, this id will be received when from BE form DB.
-    const { nodes, setNodes } = useData(); // sharing data with context hook
-    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-    const [rfInstance, setRfInstance] = useState<ReactFlowInstance | null>(null);
-    const { setViewport } = useReactFlow();
+  const reactFlowWrapper = useRef<HTMLDivElement>(null);
+  const id: string = "clo6tzkzs0000us4wi39z9qld"; // for demo purposes, now we use default id. in the production environment, this id will be received when from BE form DB.
+  const { nodes, setNodes } = useData(); // sharing data with context hook
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [rfInstance, setRfInstance] = useState<ReactFlowInstance | null>(null);
+  const { setViewport } = useReactFlow();
+  const [openCreateComment, setOpenCreateComment] = useState(false);
+  const [selectedPostIt, setSelectedPostIt] = useState<any>(null);
 
-    const { undo, redo, takeSnapshot, canUndo, canRedo, past, future } = useUndoRedo();
+  const { undo, redo, takeSnapshot, canUndo, canRedo, past, future } =
+    useUndoRedo();
 
-    useEffect(() => {
-        console.log("past", past);
-        console.log("future", future);
-    }, [past, future]);
-
-    const postItUpdate = api.postIt.postItUpdate.useMutation(); // api usage for update
-    const { data: postIt, isLoading } = api.postIt.postItSelect.useQuery({ id: id }); // api call to fetch data with trpc call from prisma orm
-    const onNodesChange = useCallback((changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)), []); // callback function to update nodes with changes
-    const onConnect = useCallback((params: Edge | Connection) => {
-        takeSnapshot();
-        setEdges((eds) => addEdge(params, eds));
-    }, [setEdges, takeSnapshot]);
-    const onDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'move';
-    }, []);
-
-    const onNodeDragStart: NodeDragHandler = useCallback(() => {
-        // 👇 make dragging a node undoable
-        takeSnapshot();
-        // 👉 you can place your event handlers here
-    }, [takeSnapshot]);
-
-    const onSelectionDragStart: SelectionDragHandler = useCallback(() => {
-        // 👇 make dragging a selection undoable
-        takeSnapshot();
-    }, [takeSnapshot]);
-
-    const onNodesDelete: OnNodesDelete = useCallback(() => {
-        // 👇 make deleting nodes undoable
-        takeSnapshot();
-    }, [takeSnapshot]);
-
-    const onEdgesDelete: OnEdgesDelete = useCallback(() => {
-        // 👇 make deleting edges undoable
-        takeSnapshot();
-    }, [takeSnapshot]);
-
-    const getId = (): string => `dndnode_${nodes.length++}`; // creating unique ids for each nodes.
-    const onDrop = useCallback(
-        (event: React.DragEvent<HTMLDivElement>) => {
-            takeSnapshot();
-            event.preventDefault();
-
-            const reactFlowBounds = reactFlowWrapper.current?.getBoundingClientRect();
-            const type = event.dataTransfer.getData('application/reactflow');
-
-            // check if the dropped element is valid
-            if (typeof type === 'undefined' || !type || !reactFlowBounds) {
-                return;
-            }
-
-            const position = rfInstance?.project({
-                x: event.clientX - (reactFlowBounds?.left || 0),
-                y: event.clientY - (reactFlowBounds?.top || 0),
-            }) ?? { x: 0, y: 0 };
-            const newNodeId = getId();
-            const newNode: PostItNodeElement = {
-                id: newNodeId,
-                type: 'postItNode', // use your custom node type
-                position,
-                data: { id: newNodeId, title: 'A new idea 💡', description: "Time to shine with your idea" }, // default content
-            };
-
-            setNodes((nds) => nds.concat([newNode]));
-        },
-        [rfInstance, takeSnapshot]
-    );
-
-    // callback function to save the data of the whole page in json format
-    const onSave = useCallback(() => {
-        if (rfInstance) {
-            const flow = rfInstance.toObject();
-            postItUpdate.mutateAsync({ id: id, state: flow }).then((res) => {
-                console.log(res); // will implement better error handlings
-            });
-        }
-    }, [rfInstance]);
-
-    // useEffect to load the data from the database
-    useEffect(() => {
-        const restoreFlow = async () => {
-            if (!postIt) return;
-            const flow = (postIt as any).state;
-
-            if (flow) {
-                const { x = 0, y = 0, zoom = 1 } = flow.viewport;
-                setNodes(flow.nodes || []);
-                setEdges(flow.edges || []);
-                setViewport({ x, y, zoom });
-            }
-        };
-
-        restoreFlow();
-    }, [postIt])
-
-    // UI element to display when data is loading from database
-    if (isLoading) {
-        return <div>Loading...</div> // will implement better UI after discussion for libs is over
+  //Obtains node ID of selected node/postit
+  const onSelectionChangeFunc = (elements: any) => {
+    if (elements.nodes.length === 1) {
+      setSelectedPostIt(elements.nodes[0].id);
+    } else {
+      setSelectedPostIt(null);
     }
-    return (
-        <>
-            <div className="" ref={reactFlowWrapper} style={{ height: '90vh' }}>
-                <Header onSave={onSave} />
-                <ReactFlow
-                    nodeTypes={nodeTypes}
-                    nodes={nodes}
-                    edges={edges}
-                    onNodesChange={onNodesChange}
-                    onEdgesChange={onEdgesChange}
-                    onConnect={onConnect}
-                    onInit={setRfInstance}
-                    onDrop={onDrop}
-                    onDragOver={onDragOver}
-                    onNodeDragStart={onNodeDragStart}
-                    onSelectionDragStart={onSelectionDragStart}
-                    onNodesDelete={onNodesDelete}
-                    onEdgesDelete={onEdgesDelete}
-                    fitView
-                    proOptions={proOptions}
-                    style={{ width: '100%', height: '100%' }}
-                >
-                    <Panel position="top-right">
-                        <Button className="border border-black p-3" onClick={onSave}>save</Button>
-                        {/* <Button className="border border-black p-3" onClick={onRestore}>restore</Button> */}
-                    </Panel>
-                    <Panel position="bottom-center">
-                        <div className="">
-                            <button disabled={canUndo} className="p-5" onClick={undo}>
-                                <span className="">⤴️</span> undo
-                            </button>
-                            <button disabled={canRedo} className="p-5" onClick={redo}>
-                                redo <span className="">⤵️</span>
-                            </button>
-                        </div>
-                    </Panel>
-                    <Controls />
-                    <MiniMap />
-                    <Background variant={"dots" as BackgroundVariant} gap={12} size={1} />
-                </ReactFlow>
+  };
+  useEffect(() => {
+    console.log("past", past);
+    console.log("future", future);
+  }, [past, future]);
 
+  const postItUpdate = api.postIt.postItUpdate.useMutation(); // api usage for update
+  const { data: postIt, isLoading } = api.postIt.postItSelect.useQuery({
+    id: id,
+  }); // api call to fetch data with trpc call from prisma orm
+  const onNodesChange = useCallback(
+    (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [],
+  ); // callback function to update nodes with changes
+  const onConnect = useCallback(
+    (params: Edge | Connection) => {
+      takeSnapshot();
+      setEdges((eds) => addEdge(params, eds));
+    },
+    [setEdges, takeSnapshot],
+  );
+  const onDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onNodeDragStart: NodeDragHandler = useCallback(() => {
+    // 👇 make dragging a node undoable
+    takeSnapshot();
+    // 👉 you can place your event handlers here
+  }, [takeSnapshot]);
+
+  const onSelectionDragStart: SelectionDragHandler = useCallback(() => {
+    // 👇 make dragging a selection undoable
+    takeSnapshot();
+  }, [takeSnapshot]);
+
+  const onNodesDelete: OnNodesDelete = useCallback(() => {
+    // 👇 make deleting nodes undoable
+    takeSnapshot();
+  }, [takeSnapshot]);
+
+  const onEdgesDelete: OnEdgesDelete = useCallback(() => {
+    // 👇 make deleting edges undoable
+    takeSnapshot();
+  }, [takeSnapshot]);
+
+  const getId = (): string => `dndnode_${nodes.length++}`; // creating unique ids for each nodes.
+  const onDrop = useCallback(
+    (event: React.DragEvent<HTMLDivElement>) => {
+      takeSnapshot();
+      event.preventDefault();
+
+      const reactFlowBounds = reactFlowWrapper.current?.getBoundingClientRect();
+      const type = event.dataTransfer.getData("application/reactflow");
+
+      // check if the dropped element is valid
+      if (typeof type === "undefined" || !type || !reactFlowBounds) {
+        return;
+      }
+
+      const position = rfInstance?.project({
+        x: event.clientX - (reactFlowBounds?.left || 0),
+        y: event.clientY - (reactFlowBounds?.top || 0),
+      }) ?? { x: 0, y: 0 };
+      const newNodeId = getId();
+      const newNode: PostItNodeElement = {
+        id: newNodeId,
+        type: "postItNode", // use your custom node type
+        position,
+        data: {
+          id: newNodeId,
+          title: "A new idea 💡",
+          description: "Time to shine with your idea",
+        }, // default content
+      };
+
+      setNodes((nds) => nds.concat([newNode]));
+    },
+    [rfInstance, takeSnapshot],
+  );
+
+  // callback function to save the data of the whole page in json format
+  const onSave = useCallback(() => {
+    if (rfInstance) {
+      const flow = rfInstance.toObject();
+      postItUpdate.mutateAsync({ id: id, state: flow }).then((res) => {
+        console.log(res); // will implement better error handlings
+      });
+    }
+  }, [rfInstance]);
+
+  // useEffect to load the data from the database
+  useEffect(() => {
+    const restoreFlow = async () => {
+      if (!postIt) return;
+      const flow = (postIt as any).state;
+
+      if (flow) {
+        const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+        setNodes(flow.nodes || []);
+        setEdges(flow.edges || []);
+        setViewport({ x, y, zoom });
+      }
+    };
+
+    restoreFlow();
+  }, [postIt]);
+
+  // UI element to display when data is loading from database
+  if (isLoading) {
+    return <div>Loading...</div>; // will implement better UI after discussion for libs is over
+  }
+  return (
+    <>
+      <div className="" ref={reactFlowWrapper} style={{ height: "90vh" }}>
+        {openCreateComment ? <Comment /> : null}
+        <Header onSave={onSave} />
+        <ReactFlow
+          nodeTypes={nodeTypes}
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onInit={setRfInstance}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          onNodeDragStart={onNodeDragStart}
+          onSelectionDragStart={onSelectionDragStart}
+          onNodesDelete={onNodesDelete}
+          onEdgesDelete={onEdgesDelete}
+          onSelectionChange={onSelectionChangeFunc}
+          fitView
+          proOptions={proOptions}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <Panel position="top-right">
+            <Button className="border border-black p-3" onClick={onSave}>
+              save
+            </Button>
+            {/* <Button className="border border-black p-3" onClick={onRestore}>restore</Button> */}
+          </Panel>
+          <Panel position="bottom-center">
+            <div className="rounded-lg bg-gray-200">
+              <button disabled={canUndo} className="p-5" onClick={undo}>
+                <span className="">⤴️</span> undo
+              </button>
+              <button disabled={canRedo} className="p-5" onClick={redo}>
+                redo <span className="">⤵️</span>
+              </button>
+              <button
+                className="p-5 disabled:opacity-50"
+                onClick={() => {
+                  setOpenCreateComment(true);
+                }}
+                disabled={!selectedPostIt}
+                title="Please select a Post It!"
+              >
+                comment
+              </button>
             </div>
-        </>
-    );
-}
+          </Panel>
+          <Controls />
+          <MiniMap />
+          <Background variant={"dots" as BackgroundVariant} gap={12} size={1} />
+        </ReactFlow>
+      </div>
+    </>
+  );
+};
+
+[
+  {
+    data: {
+      description: "Time to shine with your idea jufusdhfuasdhf",
+      id: "dndnode_0",
+      title: "A new idea 💡",
+    },
+    dragging: false,
+    height: 222,
+    id: "dndnode_0",
+    position: { x: -576.1919638709952, y: 132.5038328938325 },
+    positionAbsolute: { x: -576.1919638709952, y: 132.5038328938325 },
+    selected: true,
+    type: "postItNode",
+    width: 412,
+  },
+  {
+    data: {
+      description: "Time to shine with your idea",
+      id: "dndnode_1",
+      title: "A new idea 💡",
+    },
+    height: 222,
+    id: "dndnode_1",
+    position: { x: -347.5221217562209, y: 481.0099458661797 },
+    positionAbsolute: { x: -347.5221217562209, y: 481.0099458661797 },
+    type: "postItNode",
+    width: 412,
+  },
+];
--- src/components/_react_flow/PostItNode.tsx
+++ src/components/_react_flow/PostItNode.tsx
@@ -1,144 +1,163 @@
-
-import React, { useState, useRef, useEffect, memo } from 'react';
-import { Handle, NodeProps, Position, NodeResizer } from 'reactflow';
-import { PostItNoteItem } from '@/Types/postIt';
-import { useData } from '@/context/postItContext';
+import React, { useState, useRef, useEffect, memo } from "react";
+import { Handle, NodeProps, Position, NodeResizer } from "reactflow";
+import { PostItNoteItem } from "@/Types/postIt";
+import { useData } from "@/context/postItContext";
 import { BiPin } from "react-icons/bi";
 
 import {
-    ContextMenu,
-    ContextMenuContent,
-    ContextMenuItem,
-    ContextMenuSeparator,
-    ContextMenuShortcut,
-    ContextMenuTrigger,
-} from "@/components/ui/context-menu"
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
 
 import useCopyPaste from "@/hooks/useCopyPaste";
 
 const PostItNode: React.FC<NodeProps> = ({ data, selected }) => {
-    const [noteData, setNoteData] = useState<PostItNoteItem>(data);
-    const { nodes, setNodes } = useData();
-    const noteContainer = useRef<HTMLDivElement>(null);
-    // const contentRef = useRef<HTMLDivElement>(null);
-    // function to handle data changes in input and text area elements
-    const handleDataChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const target = e.target as HTMLInputElement & HTMLTextAreaElement;
-        //toast.message(e.target.value);
-        setNoteData({
-            ...noteData,
-            [target.name]: target.value,
-        });
-    };
+  const [noteData, setNoteData] = useState<PostItNoteItem>(data);
+  const { nodes, setNodes } = useData();
+  const noteContainer = useRef<HTMLDivElement>(null);
+  // const contentRef = useRef<HTMLDivElement>(null);
+  // function to handle data changes in input and text area elements
+  const handleDataChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const target = e.target as HTMLInputElement & HTMLTextAreaElement;
+    //toast.message(e.target.value);
+    setNoteData({
+      ...noteData,
+      [target.name]: target.value,
+    });
+  };
 
-    // function to handle data update to global state
-    const handleUpdate = (nodeId: string) => {
-        const updatedNodes = nodes.map((n) => {
-            if (n.id === nodeId) {
-                return {
-                    ...n,
-                    data: { ...n.data, ...{ title: noteData.title, description: noteData.description } },
-                };
-            }
-            return n;
-        });
-        setNodes(updatedNodes);
-    };
+  // function to handle data update to global state
+  const handleUpdate = (nodeId: string) => {
+    const updatedNodes = nodes.map((n) => {
+      if (n.id === nodeId) {
+        return {
+          ...n,
+          data: {
+            ...n.data,
+            ...{ title: noteData.title, description: noteData.description },
+          },
+        };
+      }
+      return n;
+    });
+    setNodes(updatedNodes);
+  };
 
-    // custom hook for updating data to global state with click outside event 
+  // custom hook for updating data to global state with click outside event
 
-    const { cut, copy, paste, bufferedNodes } = useCopyPaste();
+  const { cut, copy, paste, bufferedNodes } = useCopyPaste();
 
-    const canPaste = bufferedNodes.length > 0;
+  const canPaste = bufferedNodes.length > 0;
 
-    // useEffect(() => {
-    //     if (noteContainer.current && !noteContainer.current.innerHTML) {
-    //         noteContainer.current.innerHTML = noteData.label;
-    //     }
-    // }, [noteData.label]);
+  // useEffect(() => {
+  //     if (noteContainer.current && !noteContainer.current.innerHTML) {
+  //         noteContainer.current.innerHTML = noteData.label;
+  //     }
+  // }, [noteData.label]);
 
-    const handlePin = () => {
-        console.log("pin button is clicked!");
-        noteData.pinned = !noteData.pinned;
-    }
+  const handlePin = () => {
+    console.log("pin button is clicked!");
+    noteData.pinned = !noteData.pinned;
+  };
 
-    return (
-        <>
-            <ContextMenu>
-                <ContextMenuTrigger>
-                    <div ref={noteContainer} style={{ backgroundColor: '#FFDD55', padding: '10px', borderRadius: '5px' }}>
-                        <NodeResizer color="#ff0071" isVisible={selected} minWidth={100} minHeight={30} />
-                        <Handle type="target" position={Position.Left} style={{ background: '#FEB2B2', borderRadius: '4px' }} />
-                        <div className="relative" style={{ backgroundColor: data.color, padding: 10 }}>
-                            {noteData.pinned ?
-                                <div className='absolute top-1 right-1 bg-white p-1 rounded-sm'>
-                                    <BiPin />
-                                </div>
-                                : null
-                            }
-                            <div className="flex items-center justify-between space-x-8">
-                                <input
-                                    className="w-full cursor-text p-2 font-bold bg-transparent"
-                                    name="title"
-                                    value={noteData.title}
-                                    onChange={handleDataChange}
-                                />
-                                <button onClick={() => { handleUpdate(noteData.id) }}>update</button>
-                            </div>
-                            <textarea
-                                className="text-sm text-gray-600 bg-transparent w-64 h-32"
-                                name="description"
-                                value={noteData.description}
-                                onChange={handleDataChange}
-                            />
-                        </div>
-                        <Handle type="source" position={Position.Right} style={{ background: '#FEB2B2', borderRadius: '4px' }} />
-                    </div>
-                </ContextMenuTrigger>
-                <ContextMenuContent className="w-64">
-                    <ContextMenuItem inset onClick={() => copy()}>
-                        Copy
-                        <ContextMenuShortcut>⌘C</ContextMenuShortcut>
-                    </ContextMenuItem>
-                    <ContextMenuItem inset onClick={() => paste()} disabled={!canPaste}>
-                        Paste
-                        <ContextMenuShortcut>⌘V</ContextMenuShortcut>
-                    </ContextMenuItem>
-                    <ContextMenuItem inset onClick={() => cut()}>
-                        Cut
-                        <ContextMenuShortcut>⌘X</ContextMenuShortcut>
-                    </ContextMenuItem>
-                    <ContextMenuSeparator />
-                    <ContextMenuItem inset>
-                        Bring to the front
-                    </ContextMenuItem>
-                    <ContextMenuItem inset>
-                        Bring to the back
-                    </ContextMenuItem>
-                    <ContextMenuItem inset>
-                        Detach from cluster
-                    </ContextMenuItem>
-                    <ContextMenuSeparator />
-                    <ContextMenuItem inset onClick={handlePin}>
-                        Pin
-                    </ContextMenuItem>
-                    <ContextMenuItem inset onClick={handlePin}>
-                        Unpin
-                    </ContextMenuItem>
-                    <ContextMenuItem inset>
-                        Comment
-                    </ContextMenuItem>
-                    <ContextMenuItem inset>
-                        Start Vote
-                    </ContextMenuItem>
-                    <ContextMenuItem inset>
-                        Lock
-                    </ContextMenuItem>
-                </ContextMenuContent>
-            </ContextMenu >
-        </>
-
-    );
+  return (
+    <>
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <div
+            ref={noteContainer}
+            style={{
+              backgroundColor: "#FFDD55",
+              padding: "10px",
+              borderRadius: "5px",
+            }}
+          >
+            <NodeResizer
+              color="#ff0071"
+              isVisible={selected}
+              minWidth={100}
+              minHeight={30}
+            />
+            <Handle
+              type="target"
+              position={Position.Left}
+              style={{ background: "#FEB2B2", borderRadius: "4px" }}
+            />
+            <div
+              className="relative"
+              style={{ backgroundColor: data.color, padding: 10 }}
+            >
+              {noteData.pinned ? (
+                <div className="absolute right-1 top-1 rounded-sm bg-white p-1">
+                  <BiPin />
+                </div>
+              ) : null}
+              <div className="flex items-center justify-between space-x-8">
+                <input
+                  className="w-full cursor-text bg-transparent p-2 font-bold"
+                  name="title"
+                  value={noteData.title}
+                  onChange={handleDataChange}
+                />
+                <button
+                  onClick={() => {
+                    handleUpdate(noteData.id);
+                  }}
+                >
+                  update
+                </button>
+              </div>
+              <textarea
+                className="h-32 w-64 bg-transparent text-sm text-gray-600"
+                name="description"
+                value={noteData.description}
+                onChange={handleDataChange}
+              />
+            </div>
+            <Handle
+              type="source"
+              position={Position.Right}
+              style={{ background: "#FEB2B2", borderRadius: "4px" }}
+            />
+          </div>
+        </ContextMenuTrigger>
+        <ContextMenuContent className="w-64">
+          <ContextMenuItem inset onClick={() => copy()}>
+            Copy
+            <ContextMenuShortcut>⌘C</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem inset onClick={() => paste()} disabled={!canPaste}>
+            Paste
+            <ContextMenuShortcut>⌘V</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem inset onClick={() => cut()}>
+            Cut
+            <ContextMenuShortcut>⌘X</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuSeparator />
+          <ContextMenuItem inset>Bring to the front</ContextMenuItem>
+          <ContextMenuItem inset>Bring to the back</ContextMenuItem>
+          <ContextMenuItem inset>Detach from cluster</ContextMenuItem>
+          <ContextMenuSeparator />
+          <ContextMenuItem inset onClick={handlePin}>
+            Pin
+          </ContextMenuItem>
+          <ContextMenuItem inset onClick={handlePin}>
+            Unpin
+          </ContextMenuItem>
+          <ContextMenuItem inset>Comment</ContextMenuItem>
+          <ContextMenuItem inset>Start Vote</ContextMenuItem>
+          <ContextMenuItem inset>Lock</ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+    </>
+  );
 };
 
 export default memo(PostItNode);
